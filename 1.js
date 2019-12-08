92635e910963012894899b070fe0b8fae8d112a7
nguyenviettungg <nguyentung696969@gmail.com>
2019-12-08 18:18:35 +0700

    up

--- 1.js
+++ 1.js
@@ -1,4 +1,14 @@
 ﻿ $(function(){
  	new WOW().init();
+ 	var vitriinfome = $('.infome').offset().top;
+ 	console.log(vitriinfome);
+ 	$('.down,.ex').click(function(event) {
+ 		$('html,body').animate({scrollTop:vitriinfome}, 1000);
+ 		return false;
+ 	});
+ 	// $('.pr').click(function(event) {
+ 	// 	$('html,body').animate({scrollTop:}, 1000);
+ 	// 	return false;
+ 	// });
 })  
  
